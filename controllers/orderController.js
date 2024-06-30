543221dfe8f81e1577356b6f4da87319c12c9e37
nabila13 <salimanabu13@gmail.com>
2024-06-30 17:19:02 +0600

    stripe code removed

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -19,33 +19,33 @@ const placeOrder = async (req, res) => {
     });
     await newOrder.save();
     await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
-    const line_items = req.body.items.map((item) => ({
-      price_data: {
-        currency: "bdt",
-        product_data: {
-          name: item.name,
-        },
-        unit_amount: item.price * 120,
-      },
-      quantity: item.quantity,
-    }));
-    line_items.push({
-      price_data: {
-        currency: "bdt",
-        product_data: {
-          name: "Delivery Charges",
-        },
-        unit_amount: 2 * 120,
-      },
-      quantity: 1,
-    });
-    const session = await stripe.checkout.sessions.create({
-      line_items: line_items,
-      mode: "payment",
-      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
-    });
-    res.json({ success: true, session_url: session.url });
+    // const line_items = req.body.items.map((item) => ({
+    //   price_data: {
+    //     currency: "bdt",
+    //     product_data: {
+    //       name: item.name,
+    //     },
+    //     unit_amount: item.price * 120,
+    //   },
+    //   quantity: item.quantity,
+    // }));
+    // line_items.push({
+    //   price_data: {
+    //     currency: "bdt",
+    //     product_data: {
+    //       name: "Delivery Charges",
+    //     },
+    //     unit_amount: 2 * 120,
+    //   },
+    //   quantity: 1,
+    // });
+    // const session = await stripe.checkout.sessions.create({
+    //   line_items: line_items,
+    //   mode: "payment",
+    //   success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+    //   cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+    // });
+    res.json({ success: true, message: "Order Confirmed" });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: "Error" });
